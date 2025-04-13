0a6f2b868d1683d848502b5254b2eb0191c41017
1montandon <emontandon11@gmail.com>
2025-04-13 19:15:04 -0300

    Create TO-DO add

--- chapter_3/src/middleware/authMiddleware.js
+++ chapter_3/src/middleware/authMiddleware.js
@@ -10,6 +10,7 @@ function authMiddleware(req, res, next){
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
         if (err) {return res.status(401).json({message: "Invalid Token"})}
         req.userId = decoded.id
+        console.log(req.userId)
         next()
     })
 }
